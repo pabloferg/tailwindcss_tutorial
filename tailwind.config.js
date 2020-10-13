fb418bde9cc8025589303863d43484d154d8de51
pabloferg <pablofergui1@gmail.com>
2020-10-13 19:55:13 +0000

    check size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+          'brand-blue': '#1992d4',
+      },
+        spacing: {
+          '72': '18rem',
+      }
+    },
   },
   variants: {
       backgroundColor: ['responsive', 'hover', 'focus', 'active']
